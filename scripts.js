4740d9814b3bdfc241d6368fa1542fe009771bff
iuri <ijiki16@freeuni.edu.ge>
2020-10-14 14:07:19 +0400

    remove vs code files from repo & add basic rec

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const app = document.getElementById('root')
+
 var request = new XMLHttpRequest();
 
 request.open('GET', 'https://api.adjaranet.com/api/v1/movies', true);
@@ -7,13 +9,32 @@ request.onload = function () {
 
   if (request.status >= 200 && request.status < 400) {
     data.data.forEach((movie) => {
-      console.log(movie.secondaryName);
+      //console.log(movie.secondaryName)
+
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.secondaryName
+
+      const img = document.createElement('img')
+
+      //console.log(movie.covers.data)
+      img.src = movie.covers.data[510]
+      //movie.description = movie.description.substring(0, 300)
+
+      card.appendChild(img)
+      card.appendChild(h1)
+
+      app.appendChild(card)
+
     })
-    console.log(data.data)
-    console.log(data.meta)
+
+    //console.log(data.data)
+    //console.log(data.meta)
   } else {
-    console.log('error');
+    console.log('error')
   }
 }
 
-request.send();
+request.send()
