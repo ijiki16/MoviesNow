79b2a0d9d9042134e2189fdcec5489d9784bb3e9
iuri <ijiki16@freeuni.edu.ge>
2020-10-15 16:51:14 +0400

    add favicon

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@ const app = document.getElementById('root')
 
 var request = new XMLHttpRequest();
 
-request.open('GET', 'https://api.adjaranet.com/api/v1/movies', true);
+request.open('GET', 'https://api.adjaranet.com/api/v1/movies/top?type=movie&period=day&page=1&per_page=20&filters%5Bwith_actors%5D=3&filters%5Bwith_directors%5D=1&source=adjaranet', true);
 
 request.onload = function () {
   var data = JSON.parse(this.response);
-
+  
   if (request.status >= 200 && request.status < 400) {
     data.data.forEach((movie) => {
-      //console.log(movie.secondaryName)
+      console.log(movie.poster)
 
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
@@ -18,9 +18,10 @@ request.onload = function () {
       h1.textContent = movie.secondaryName
 
       const img = document.createElement('img')
+      img.width = 200
 
       //console.log(movie.covers.data)
-      img.src = movie.covers.data[510]
+      img.src = movie.posters.data[240]
       //movie.description = movie.description.substring(0, 300)
 
       card.appendChild(img)
