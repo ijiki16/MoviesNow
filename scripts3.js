385df9e0bf881d607973d6ffff5f34c92e082ac5
iuri <ijiki16@freeuni.edu.ge>
2020-11-29 16:42:22 +0400

    fix scroll bar

--- scripts3.js
+++ scripts3.js
@@ -2,8 +2,23 @@ const app3 = document.getElementById('root3')
 
 var request3 = new XMLHttpRequest();
 
-request3.open('GET', 'https://api.adjaranet.com/api/v1/movies/top?type=movie&period=month&page=1&source=adjaranet', true);
+request3.open('GET', 'https://api.adjaranet.com/api/v1/movies', true);
 
+// this.response.setHeader("Access-Control-Allow-Origin", "*");
+// this.response.setHeader("Access-Control-Allow-Credentials", "true");
+// this.response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+// this.response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+request3.setHeader("page", "1");
+// request3.setRequestHeader("per_page", "20");
+// ?filters%5Blanguage%5D=GEO&filters%5Btype%5D=series&filters%5Bwith_actors%5D=3&filters%5Bwith_directors%5D=1&filters%5Bwith_files%5D=yes&sort=-upload_date&source=adjaranet
+// request3.setRequestHeader("filters%5Blanguage%5D", "GEO");
+// request3.setRequestHeader("filters%5Btype%5D", "series");
+// request3.setRequestHeader("filters%5Bwith_actors%5D", "3");
+// request3.setRequestHeader("filters%5Bwith_directors%5D", "1");
+// request3.setRequestHeader("filters%5Bwith_files%5D", "yes");
+// request3.setRequestHeader("sort", "-upload_date");
+// request3.setRequestHeader("source", "adjaranet");
 request3.onload = function () {
   var data = JSON.parse(this.response);
   
